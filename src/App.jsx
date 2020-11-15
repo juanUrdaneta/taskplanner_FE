1c64ccde7125b5cf769c8ea63853f1aa463f3d8f
juanUrdaneta <juankorus@gmail.com>
2020-11-15 20:08:07 -0300

    added new task to the routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./Components/Login";
 import { Switch, Route } from "react-router-dom";
 import Taskboard from "./Components/TaskBoard/Taskboard";
 import Navbar from "./Components/TaskBoard/Navbar";
+import NewTaskView from "./Components/NewTask/NewTaskView";
 
 function App() {
   //logic
@@ -13,6 +14,8 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/taskboard" component={Taskboard} />
+        <Route path="/newtask" component={NewTaskView} />
         <Route path="/" component={Taskboard} />
       </Switch>
     </div>
